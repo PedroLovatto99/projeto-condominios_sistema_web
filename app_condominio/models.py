bd195d3a6e4c6cab9e85047d0cbb908c584fc369
Pedro Leal Lovatto <pedrolovatto99@gmail.com>
2024-09-22 14:51:09 -0300

    ultimas correções nos models

--- app_condominio/models.py
+++ app_condominio/models.py
@@ -8,7 +8,7 @@ class Bloco(models.Model):
         ("#00B050", "Verde"),
     ]
     numero = models.PositiveIntegerField(verbose_name="Número")
-    cor = models.CharField(max_length=7, choices=CORES_SELECT, null=True, blank=True)
+    cor = models.CharField(max_length=7, choices=CORES_SELECT)
 
     class Meta:
         verbose_name = "Bloco"
@@ -31,15 +31,15 @@ class Apartamento(models.Model):
 class Residente(models.Model):
     nome = models.CharField(max_length=100)
     apartamento = models.ForeignKey(Apartamento, on_delete=models.PROTECT)
-    cpf_cnpj = models.CharField(verbose_name="CPF/CNPJ", max_length=18, null=True, blank=True)
-    telefone = models.CharField(verbose_name="Telefone", max_length=15, null=True, blank=True)
-    email = models.EmailField(verbose_name="E-mail", null=True, blank=True)
-    data_inicio = models.DateField(verbose_name="Data de Início do contrato", null=True, blank=True)
-    data_fim = models.DateField(verbose_name="Data de Término do contrato", null=True, blank=True)
+    cpf_cnpj = models.CharField(verbose_name="CPF/CNPJ", max_length=18)
+    telefone = models.CharField(verbose_name="Telefone", max_length=15)
+    email = models.EmailField(verbose_name="E-mail")
+    data_inicio = models.DateField(verbose_name="Data de Início do contrato")
+    data_fim = models.DateField(verbose_name="Data de Término do contrato")
     prorrogacao = models.DateField(verbose_name="Data de Prorrogação do Contrato", null=True, blank=True)
-    numero_cadastro = models.IntegerField(verbose_name="Numero de Cadastro", null=True, blank=True)
-    valor_aluguel = models.FloatField(verbose_name="Valor do Aluguel", null=True, blank=True)
-    valor_condominio = models.FloatField(verbose_name="Valor do Condomínio", null=True, blank=True)
+    numero_cadastro = models.IntegerField(verbose_name="Numero de Cadastro")
+    valor_aluguel = models.FloatField(verbose_name="Valor do Aluguel")
+    valor_condominio = models.FloatField(verbose_name="Valor do Condomínio")
     outros = models.FloatField(verbose_name="Outros valores a pagar", null=True, blank=True)
     valor_gas = models.FloatField(verbose_name="Valor do Gás", null=True, blank=True)
     data_vencimento = models.DateField(verbose_name="Data de Vencimento do boleto", null=True, blank=True)
