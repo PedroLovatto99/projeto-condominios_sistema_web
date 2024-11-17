cebdcf8d762f89bd541146c8d74eec7c73cfeeab
Pedro Leal Lovatto <pedrolovatto99@gmail.com>
2024-11-17 19:14:35 -0300

    apagado diretorio utils

--- app_condominio/admin.py
+++ app_condominio/admin.py
@@ -2,17 +2,21 @@ from django.contrib import admin
 from simple_history.admin import SimpleHistoryAdmin
 from .models import Bloco, Apartamento, Residente
 
+
 class ResidenteAdmin(SimpleHistoryAdmin):
     list_display = ('nome', 'apartamento', 'cpf_cnpj', 'telefone', 'email')
     list_filter = ('apartamento__bloco',)
-    history_list_display = ('history_date', 'history_type', 'history_user')
+    # history_list_display = ('history_date', 'history_type', 'history_user')
+
 
 class ApartamentoAdmin(SimpleHistoryAdmin):
     list_display = ('numero', 'bloco')
     list_filter = ('bloco', 'numero')
 
+
 class BlocoAdmin(SimpleHistoryAdmin):
     list_display = ('numero', 'cor')
+
 
 admin.site.register(Bloco, BlocoAdmin)
 admin.site.register(Apartamento, ApartamentoAdmin)
