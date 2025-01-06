6f44f146a2a1528ef4deb50a02920c631a14cb33
PedroLovatto99 <pedrolovatto99@gmail.com>
2025-01-06 19:21:57 +0000

    fix: mudanças no settings.py

--- projeto_condominio/settings.py
+++ projeto_condominio/settings.py
@@ -68,23 +68,22 @@ WSGI_APPLICATION = "projeto_condominio.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+#DATABASES = {
+ #   "default": {
+  #      "ENGINE": "django.db.backends.sqlite3",
+   #     "NAME": BASE_DIR / "db.sqlite3",
+    #}
+#}
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+     'default': {
+         'ENGINE': 'django.db.backends.postgresql',
+         'NAME': str(os.getenv('NAME_PG')),
+         'USER': str(os.getenv('USER_PG')),
+         'PASSWORD': str(os.getenv('PASSWORD_PG')),
+         'HOST': str(os.getenv('HOST_PG')),
+         'PORT': str(os.getenv('PORT_PG')),
+     }
 }
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': str(os.getenv('NAME_PG')),
-#         'USER': str(os.getenv('USER_PG')),
-#         'PASSWORD': str(os.getenv('PASSWORD_PG')),
-#         'HOST': str(os.getenv('HOST_PG')),
-#         'PORT': str(os.getenv('PORT_PG')),
-#     }
-# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
@@ -127,6 +126,8 @@ STATICFILES_DIRS = [
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
